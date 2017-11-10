8ccb1048ef6c772f2c8719ee068fd1e811ea760a
Daniel Pombo <pombodaniel10@gmail.com>
2017-11-09 21:23:41 -0500

    Nuevos topics añadidos

--- middlewares/mqtt.js
+++ middlewares/mqtt.js
@@ -1,17 +1,19 @@
 var mqtt = require('mqtt');
 
-var client  = mqtt.connect('mqtt://m12.cloudmqtt.com',{
-  port: 12489,
-  clientId: "Severo perro pirobo",
-  username:"xbvmyoxh",
-  password:"nJzjyl5r-7GD"
-});
+var options = {
+    port: 12489,
+    username: 'xbvmyoxh',
+    password: 'nJzjyl5r-7GD',
+};
+var client = mqtt.connect('mqtt://m12.cloudmqtt.com', options);
 
 client.on('connect', function () {
   console.log("Conectado al broker.");
   client.subscribe('outStepper');
   client.subscribe('inStepper');
   client.subscribe('prrito');
+  client.subscribe('encenderFoco');
+  client.subscribe('foco_cambio');
   client.subscribe('foco_estado');
 });
 
