05a48f6f0bebfe276b87485fe22138688b0b67fb
Daniel Pombo <pombodaniel10@gmail.com>
2017-11-19 11:38:45 -0500

    Actualización a Angular 5

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 import {RouterModule, Routes} from '@angular/router';
 import {FlashMessagesModule} from 'angular2-flash-messages';
 import { Observable, Subject } from 'rxjs/Rx';
@@ -63,7 +63,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule,
     ChartsModule,
