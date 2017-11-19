092bf212f08b0eafeaf3ed8cfb9f605e7c9990a8
Daniel Pombo <pombodaniel10@gmail.com>
2017-11-19 11:39:51 -0500

    Añadido atributo id a User

--- angular-src/src/app/components/admin/register/register.component.ts
+++ angular-src/src/app/components/admin/register/register.component.ts
@@ -5,16 +5,16 @@ import {AuthService} from '../../../../app/services/auth.service';
 import {FlashMessagesService} from 'angular2-flash-messages';
 import {Router} from '@angular/router';
 import {User} from './user';
-import{Observable} from 'rxjs/Rx';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent{
-  users : User[];
-  user: User = new User('','','','',false);
+export class RegisterComponent {
+  users: User[];
+  user: User = new User('', '', '', '', '', false);
   form: FormGroup;
 
   constructor(
@@ -24,49 +24,49 @@ export class RegisterComponent{
     private router: Router,
   ) {
     this.form = new FormGroup({
-      'name': new FormControl('', [Validators.required,Validators.minLength(3)]),
-      'username': new FormControl('',[Validators.required,Validators.minLength(5)]),
+      'name': new FormControl('', [Validators.required, Validators.minLength(3)]),
+      'username': new FormControl('', [Validators.required, Validators.minLength(5)]),
       'email': new FormControl('', Validators.required),
-      'password': new FormControl('', [Validators.required,Validators.minLength(8)]),
+      'password': new FormControl('', [Validators.required, Validators.minLength(8)]),
       'password2': new FormControl(),
       'isAdmin': new FormControl(false)
-    })
+    });
 
     this.form.controls['password2'].setValidators(
     [  Validators.required,
       this.noIguales.bind(this.form)]
-    )
+    );
 
    }
 
-  noIguales(control:FormControl):{[s:string]:boolean}{
-    let forma:any= this;
-    if(control.value !== forma.controls['password'].value){
+  noIguales(control: FormControl): {[s: string]: boolean} {
+    const forma: any = this;
+    if (control.value !== forma.controls['password'].value) {
       return {
-        noIguales:true
-      }
+        noIguales: true
+      };
     }
     return null;
   }
 
-  onRegisterSubmit(){
+  onRegisterSubmit() {
 
     this.user = this.form.value;
 
-    //Validate Email
-    if(!this.validateService.validateEmail(this.user.email)){
-      this.flashMessage.show("Por favor, ingrese un correo valido",{cssClass: 'alert-danger', timeout:3000});
+    // Validate Email
+    if (!this.validateService.validateEmail(this.user.email)) {
+      this.flashMessage.show('Por favor, ingrese un correo valido', {cssClass: 'alert-danger', timeout: 3000});
       return false;
     }
 
-    //Register user
+    // Register user
     this.authService.registerUser(this.user)
       .then((data: any) => {
-      if(data.success){
-        this.flashMessage.show("¡Registro exitoso!",{cssClass: 'alert-success', timeout:3000});
+      if (data.success) {
+        this.flashMessage.show('¡Registro exitoso!', {cssClass: 'alert-success', timeout: 3000});
         this.router.navigate(['admin']);
-      }else{
-        this.flashMessage.show(data.msg,{cssClass: 'alert-danger', timeout:3000});
+      } else {
+        this.flashMessage.show(data.msg, {cssClass: 'alert-danger', timeout: 3000});
         this.router.navigate(['register']);
       }
     });
--- angular-src/src/app/components/admin/register/user.ts
+++ angular-src/src/app/components/admin/register/user.ts
@@ -1,6 +1,7 @@
 export class User {
 
   constructor(
+    public _id: string,
     public name: string,
     public username: string,
     public email: string,
